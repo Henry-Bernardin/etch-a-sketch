fc59896713b6c65179a436cc2d39d287a698535e
Henry Bernardin <henry.bernardin@gmail.com>
2025-03-23 18:51:17 -0400

    feat: add addDiv function
    -addDiv function creates 14 new div elements on the body of the html
    -New Div text is written within each div to help debug

---
+++ script.js
@@ -0,0 +1,19 @@
+function addDiv() {
+  //create a new div element
+  const newDiv = document.createElement("div");
+
+  //give it content
+  const newContent = document.createTextNode("New Div");
+
+  //add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  //add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("container");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+//loop it 15 times
+for (let i = 1; i < 15; i++) {
+  addDiv();
+}
